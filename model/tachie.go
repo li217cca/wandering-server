5c016b4de3cd1f7e683adcd854ddeb841d58bfef
ljrscls <446811346@qq.com>
2017-08-26 00:24:54 +0800

    调整文件结构

--- model/tachie.go
+++ model/tachie.go
@@ -1,17 +1,15 @@
 package model
 
-import "image"
-
 type Tachie struct {
-	ID              int    `json:"id"`
-	URL             string `json:"url"`
-	HeadTransform   string `json:"head_transform"`
-	BigTransform    string `json:"big_transform"`
-	BattleTransform string `json:"battle_transform"`
-	CardTransform   string `json:"card_transform"`
+	ID              int             `json:"id"`
+	URL             string          `json:"url"`
+	HeadTransform   TachieTransform `json:"head_transform"`
+	BigTransform    TachieTransform `json:"big_transform"`
+	BattleTransform TachieTransform `json:"battle_transform"`
+	CardTransform   TachieTransform `json:"card_transform"`
 }
 
 type TachieTransform struct {
-	Size image.Point
-	Position image.Point
+	Size     string `json:"size"`
+	Position string `json:"position"`
 }
